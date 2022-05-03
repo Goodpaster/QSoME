430f6a654fdff77f8811ca48fe5d8ab0e3a6781e
DanSGraham <dan.s.graham@gmail.com>
2022-05-03 17:30:26 -0500

    cleaned up passing density tests

--- tests/grad_tests.py
+++ tests/grad_tests.py
@@ -97,138 +97,9 @@ class TestEnvSubSystemGradients(unittest.TestCase):
         os_mol.build()
         self.os_sub1_twoatm_mol1 = os_mol
 
-    #def test_rhf_den_grad(self):
+    def test_rhf_den_grad(self):
 
-    #    env_method = 'hf'
-    #    hl_method = 'ccsd'
-    #    subsys1 = cluster_subsystem.ClusterHLSubSystem(self.cs_small_mol1, env_method, hl_method)
-    #    env_subsys1 = cluster_subsystem.ClusterEnvSubSystem(self.cs_small_mol2, env_method)
-    #    sup_mol11 = helpers.concat_mols([self.cs_small_mol1, self.cs_small_mol2])
-    #    fs_scf_obj_1 = helpers.gen_scf_obj(sup_mol11, env_method)
-    #    supersystem = cluster_supersystem.ClusterSuperSystem([subsys1, env_subsys1], env_method, fs_scf_obj_1)
-    #    supersystem.init_density()
-    #    supersystem.freeze_and_thaw()
-    #    supersystem.get_supersystem_nuc_grad()
-    #    ao_dmat = supersystem.get_sub_den_grad()
-
-    #    atmlist = range(supersystem.mol.natm)
-    #    for atm in atmlist:
-    #        for dim in range(3):
-    #            dim_diff = 0.0001
-    #            if atm in supersystem.atm_sub2sup[0]:
-    #                coord_0 = self.cs_small_mol1.atom_coords()
-    #                coord_0[atm][dim] = coord_0[atm][dim] - dim_diff
-    #                mol0 = self.cs_small_mol1.set_geom_(coord_0, 'B', inplace=False)
-    #                env_mol0 = copy.copy(self.cs_small_mol2)
-
-    #                coord_2 = self.cs_small_mol1.atom_coords()
-    #                coord_2[atm][dim] = coord_2[atm][dim] + dim_diff
-    #                mol2 = self.cs_small_mol1.set_geom_(coord_2, 'B', inplace=False)
-    #                env_mol2 = copy.copy(self.cs_small_mol2)
-    #            else:
-    #                sub1_atms = supersystem.atm_sub2sup[0][-1] + 1
-    #                coord_0 = self.cs_small_mol2.atom_coords()
-    #                coord_0[atm-sub1_atms][dim] = coord_0[atm-sub1_atms][dim] - dim_diff
-    #                env_mol0 = self.cs_small_mol2.set_geom_(coord_0, 'B', inplace=False)
-    #                mol0 = copy.copy(self.cs_small_mol1)
-
-    #                coord_2 = self.cs_small_mol2.atom_coords()
-    #                coord_2[atm-sub1_atms][dim] = coord_2[atm-sub1_atms][dim] + dim_diff
-    #                env_mol2 = self.cs_small_mol2.set_geom_(coord_2, 'B', inplace=False)
-    #                mol2 = copy.copy(self.cs_small_mol1)
-
-    #            subsys0 = cluster_subsystem.ClusterHLSubSystem(mol0, env_method, hl_method)
-    #            env_subsys0 = cluster_subsystem.ClusterEnvSubSystem(env_mol0, env_method)
-    #            sup_mol00 = helpers.concat_mols([mol0, env_mol0])
-    #            fs_scf_obj0 = helpers.gen_scf_obj(sup_mol00, env_method)
-    #            supersystem0 = cluster_supersystem.ClusterSuperSystem([subsys0, env_subsys0], env_method, fs_scf_obj0)
-    #            supersystem0.init_density()
-    #            supersystem0.freeze_and_thaw()
-
-    #            subsys2 = cluster_subsystem.ClusterHLSubSystem(mol2, env_method, hl_method)
-    #            env_subsys2 = cluster_subsystem.ClusterEnvSubSystem(env_mol2, env_method)
-    #            sup_mol22 = helpers.concat_mols([mol2, env_mol2])
-    #            fs_scf_obj2 = helpers.gen_scf_obj(sup_mol22, env_method)
-    #            supersystem2 = cluster_supersystem.ClusterSuperSystem([subsys2, env_subsys2], env_method, fs_scf_obj2)
-    #            supersystem2.init_density()
-    #            supersystem2.freeze_and_thaw()
-
-    #            #Num density grad terms
-    #            num_sub1_den_grad = (subsys2.get_dmat() - subsys0.get_dmat())/(dim_diff*2.)
-    #            num_sub2_den_grad = (env_subsys2.get_dmat() - env_subsys0.get_dmat())/(dim_diff*2.)
-    #            
-    #            s2s = supersystem.sub2sup
-    #            print (np.max(np.abs(ao_dmat[0][atm][dim] - num_sub1_den_grad)))
-    #            print (np.max(np.abs(ao_dmat[1][atm][dim] - num_sub2_den_grad)))
-    #            self.assertTrue(np.allclose(ao_dmat[0][atm][dim], num_sub1_den_grad, atol=1e-5))
-    #            self.assertTrue(np.allclose(ao_dmat[1][atm][dim], num_sub2_den_grad, atol=1e-5))
-
-
-    #    subsys1 = cluster_subsystem.ClusterHLSubSystem(self.cs_twoatm_mol1, env_method, hl_method)
-    #    env_subsys1 = cluster_subsystem.ClusterEnvSubSystem(self.cs_twoatm_mol2, env_method)
-    #    sup_mol11 = helpers.concat_mols([self.cs_twoatm_mol1, self.cs_twoatm_mol2])
-    #    fs_scf_obj_1 = helpers.gen_scf_obj(sup_mol11, env_method)
-    #    supersystem = cluster_supersystem.ClusterSuperSystem([subsys1, env_subsys1], env_method, fs_scf_obj_1)
-    #    supersystem.init_density()
-    #    supersystem.freeze_and_thaw()
-    #    supersystem.get_supersystem_nuc_grad()
-    #    ao_dmat = supersystem.get_sub_den_grad()
-
-    #    atmlist = range(supersystem.mol.natm)
-    #    for atm in atmlist:
-    #        for dim in range(3):
-    #            dim_diff = 0.0001
-    #            if atm in supersystem.atm_sub2sup[0]:
-    #                coord_0 = self.cs_twoatm_mol1.atom_coords()
-    #                coord_0[atm][dim] = coord_0[atm][dim] - dim_diff
-    #                mol0 = self.cs_twoatm_mol1.set_geom_(coord_0, 'B', inplace=False)
-    #                env_mol0 = copy.copy(self.cs_twoatm_mol2)
-
-    #                coord_2 = self.cs_twoatm_mol1.atom_coords()
-    #                coord_2[atm][dim] = coord_2[atm][dim] + dim_diff
-    #                mol2 = self.cs_twoatm_mol1.set_geom_(coord_2, 'B', inplace=False)
-    #                env_mol2 = copy.copy(self.cs_twoatm_mol2)
-    #            else:
-    #                sub1_atms = supersystem.atm_sub2sup[0][-1] + 1
-    #                coord_0 = self.cs_twoatm_mol2.atom_coords()
-    #                coord_0[atm-sub1_atms][dim] = coord_0[atm-sub1_atms][dim] - dim_diff
-    #                env_mol0 = self.cs_twoatm_mol2.set_geom_(coord_0, 'B', inplace=False)
-    #                mol0 = copy.copy(self.cs_twoatm_mol1)
-
-    #                coord_2 = self.cs_twoatm_mol2.atom_coords()
-    #                coord_2[atm-sub1_atms][dim] = coord_2[atm-sub1_atms][dim] + dim_diff
-    #                env_mol2 = self.cs_twoatm_mol2.set_geom_(coord_2, 'B', inplace=False)
-    #                mol2 = copy.copy(self.cs_twoatm_mol1)
-
-    #            subsys0 = cluster_subsystem.ClusterHLSubSystem(mol0, env_method, hl_method)
-    #            env_subsys0 = cluster_subsystem.ClusterEnvSubSystem(env_mol0, env_method)
-    #            sup_mol00 = helpers.concat_mols([mol0, env_mol0])
-    #            fs_scf_obj0 = helpers.gen_scf_obj(sup_mol00, env_method)
-    #            supersystem0 = cluster_supersystem.ClusterSuperSystem([subsys0, env_subsys0], env_method, fs_scf_obj0)
-    #            supersystem0.init_density()
-    #            supersystem0.freeze_and_thaw()
-
-    #            subsys2 = cluster_subsystem.ClusterHLSubSystem(mol2, env_method, hl_method)
-    #            env_subsys2 = cluster_subsystem.ClusterEnvSubSystem(env_mol2, env_method)
-    #            sup_mol22 = helpers.concat_mols([mol2, env_mol2])
-    #            fs_scf_obj2 = helpers.gen_scf_obj(sup_mol22, env_method)
-    #            supersystem2 = cluster_supersystem.ClusterSuperSystem([subsys2, env_subsys2], env_method, fs_scf_obj2)
-    #            supersystem2.init_density()
-    #            supersystem2.freeze_and_thaw()
-
-    #            #Num density grad terms
-    #            num_sub1_den_grad = (subsys2.get_dmat() - subsys0.get_dmat())/(dim_diff*2.)
-    #            num_sub2_den_grad = (env_subsys2.get_dmat() - env_subsys0.get_dmat())/(dim_diff*2.)
-    #            
-    #            s2s = supersystem.sub2sup
-    #            print (np.max(np.abs(ao_dmat[0][atm][dim] - num_sub1_den_grad)))
-    #            print (np.max(np.abs(ao_dmat[1][atm][dim] - num_sub2_den_grad)))
-    #            self.assertTrue(np.allclose(ao_dmat[0][atm][dim], num_sub1_den_grad, atol=1e-5))
-    #            self.assertTrue(np.allclose(ao_dmat[1][atm][dim], num_sub2_den_grad, atol=1e-5))
-
-    def test_rks_den_grad(self):
-
-        env_method = 'lda'
+        env_method = 'hf'
         hl_method = 'ccsd'
         subsys1 = cluster_subsystem.ClusterHLSubSystem(self.cs_small_mol1, env_method, hl_method)
         env_subsys1 = cluster_subsystem.ClusterEnvSubSystem(self.cs_small_mol2, env_method)
@@ -237,10 +108,8 @@ class TestEnvSubSystemGradients(unittest.TestCase):
         supersystem = cluster_supersystem.ClusterSuperSystem([subsys1, env_subsys1], env_method, fs_scf_obj_1)
         supersystem.init_density()
         supersystem.freeze_and_thaw()
-
         supersystem.get_supersystem_nuc_grad()
         ao_dmat = supersystem.get_sub_den_grad()
-
 
         atmlist = range(supersystem.mol.natm)
         for atm in atmlist:
@@ -289,12 +158,143 @@ class TestEnvSubSystemGradients(unittest.TestCase):
                 num_sub2_den_grad = (env_subsys2.get_dmat() - env_subsys0.get_dmat())/(dim_diff*2.)
                 
                 s2s = supersystem.sub2sup
+                print (np.max(np.abs(ao_dmat[0][atm][dim] - num_sub1_den_grad)))
+                print (np.max(np.abs(ao_dmat[1][atm][dim] - num_sub2_den_grad)))
+                self.assertTrue(np.allclose(ao_dmat[0][atm][dim], num_sub1_den_grad, atol=1e-5))
+                self.assertTrue(np.allclose(ao_dmat[1][atm][dim], num_sub2_den_grad, atol=1e-5))
+
+
+        subsys1 = cluster_subsystem.ClusterHLSubSystem(self.cs_twoatm_mol1, env_method, hl_method)
+        env_subsys1 = cluster_subsystem.ClusterEnvSubSystem(self.cs_twoatm_mol2, env_method)
+        sup_mol11 = helpers.concat_mols([self.cs_twoatm_mol1, self.cs_twoatm_mol2])
+        fs_scf_obj_1 = helpers.gen_scf_obj(sup_mol11, env_method)
+        supersystem = cluster_supersystem.ClusterSuperSystem([subsys1, env_subsys1], env_method, fs_scf_obj_1)
+        supersystem.init_density()
+        supersystem.freeze_and_thaw()
+        supersystem.get_supersystem_nuc_grad()
+        ao_dmat = supersystem.get_sub_den_grad()
+
+        atmlist = range(supersystem.mol.natm)
+        for atm in atmlist:
+            for dim in range(3):
+                dim_diff = 0.0001
+                if atm in supersystem.atm_sub2sup[0]:
+                    coord_0 = self.cs_twoatm_mol1.atom_coords()
+                    coord_0[atm][dim] = coord_0[atm][dim] - dim_diff
+                    mol0 = self.cs_twoatm_mol1.set_geom_(coord_0, 'B', inplace=False)
+                    env_mol0 = copy.copy(self.cs_twoatm_mol2)
+
+                    coord_2 = self.cs_twoatm_mol1.atom_coords()
+                    coord_2[atm][dim] = coord_2[atm][dim] + dim_diff
+                    mol2 = self.cs_twoatm_mol1.set_geom_(coord_2, 'B', inplace=False)
+                    env_mol2 = copy.copy(self.cs_twoatm_mol2)
+                else:
+                    sub1_atms = supersystem.atm_sub2sup[0][-1] + 1
+                    coord_0 = self.cs_twoatm_mol2.atom_coords()
+                    coord_0[atm-sub1_atms][dim] = coord_0[atm-sub1_atms][dim] - dim_diff
+                    env_mol0 = self.cs_twoatm_mol2.set_geom_(coord_0, 'B', inplace=False)
+                    mol0 = copy.copy(self.cs_twoatm_mol1)
+
+                    coord_2 = self.cs_twoatm_mol2.atom_coords()
+                    coord_2[atm-sub1_atms][dim] = coord_2[atm-sub1_atms][dim] + dim_diff
+                    env_mol2 = self.cs_twoatm_mol2.set_geom_(coord_2, 'B', inplace=False)
+                    mol2 = copy.copy(self.cs_twoatm_mol1)
+
+                subsys0 = cluster_subsystem.ClusterHLSubSystem(mol0, env_method, hl_method)
+                env_subsys0 = cluster_subsystem.ClusterEnvSubSystem(env_mol0, env_method)
+                sup_mol00 = helpers.concat_mols([mol0, env_mol0])
+                fs_scf_obj0 = helpers.gen_scf_obj(sup_mol00, env_method)
+                supersystem0 = cluster_supersystem.ClusterSuperSystem([subsys0, env_subsys0], env_method, fs_scf_obj0)
+                supersystem0.init_density()
+                supersystem0.freeze_and_thaw()
+
+                subsys2 = cluster_subsystem.ClusterHLSubSystem(mol2, env_method, hl_method)
+                env_subsys2 = cluster_subsystem.ClusterEnvSubSystem(env_mol2, env_method)
+                sup_mol22 = helpers.concat_mols([mol2, env_mol2])
+                fs_scf_obj2 = helpers.gen_scf_obj(sup_mol22, env_method)
+                supersystem2 = cluster_supersystem.ClusterSuperSystem([subsys2, env_subsys2], env_method, fs_scf_obj2)
+                supersystem2.init_density()
+                supersystem2.freeze_and_thaw()
+
+                #Num density grad terms
+                num_sub1_den_grad = (subsys2.get_dmat() - subsys0.get_dmat())/(dim_diff*2.)
+                num_sub2_den_grad = (env_subsys2.get_dmat() - env_subsys0.get_dmat())/(dim_diff*2.)
+                
+                s2s = supersystem.sub2sup
+                print (np.max(np.abs(ao_dmat[0][atm][dim] - num_sub1_den_grad)))
+                print (np.max(np.abs(ao_dmat[1][atm][dim] - num_sub2_den_grad)))
+                self.assertTrue(np.allclose(ao_dmat[0][atm][dim], num_sub1_den_grad, atol=1e-5))
+                self.assertTrue(np.allclose(ao_dmat[1][atm][dim], num_sub2_den_grad, atol=1e-5))
+
+    def test_rks_den_grad(self):
+
+        env_method = 'lda'
+        hl_method = 'ccsd'
+        subsys1 = cluster_subsystem.ClusterHLSubSystem(self.cs_small_mol1, env_method, hl_method)
+        env_subsys1 = cluster_subsystem.ClusterEnvSubSystem(self.cs_small_mol2, env_method)
+        sup_mol11 = helpers.concat_mols([self.cs_small_mol1, self.cs_small_mol2])
+        fs_scf_obj_1 = helpers.gen_scf_obj(sup_mol11, env_method, grid_level=5)
+        supersystem = cluster_supersystem.ClusterSuperSystem([subsys1, env_subsys1], env_method, fs_scf_obj_1)
+        supersystem.init_density()
+        supersystem.freeze_and_thaw()
+
+        supersystem.get_supersystem_nuc_grad()
+        ao_dmat = supersystem.get_sub_den_grad()
+
+
+        atmlist = range(supersystem.mol.natm)
+        for atm in atmlist:
+            for dim in range(3):
+                dim_diff = 0.0001
+                if atm in supersystem.atm_sub2sup[0]:
+                    coord_0 = self.cs_small_mol1.atom_coords()
+                    coord_0[atm][dim] = coord_0[atm][dim] - dim_diff
+                    mol0 = self.cs_small_mol1.set_geom_(coord_0, 'B', inplace=False)
+                    env_mol0 = copy.copy(self.cs_small_mol2)
+
+                    coord_2 = self.cs_small_mol1.atom_coords()
+                    coord_2[atm][dim] = coord_2[atm][dim] + dim_diff
+                    mol2 = self.cs_small_mol1.set_geom_(coord_2, 'B', inplace=False)
+                    env_mol2 = copy.copy(self.cs_small_mol2)
+                else:
+                    sub1_atms = supersystem.atm_sub2sup[0][-1] + 1
+                    coord_0 = self.cs_small_mol2.atom_coords()
+                    coord_0[atm-sub1_atms][dim] = coord_0[atm-sub1_atms][dim] - dim_diff
+                    env_mol0 = self.cs_small_mol2.set_geom_(coord_0, 'B', inplace=False)
+                    mol0 = copy.copy(self.cs_small_mol1)
+
+                    coord_2 = self.cs_small_mol2.atom_coords()
+                    coord_2[atm-sub1_atms][dim] = coord_2[atm-sub1_atms][dim] + dim_diff
+                    env_mol2 = self.cs_small_mol2.set_geom_(coord_2, 'B', inplace=False)
+                    mol2 = copy.copy(self.cs_small_mol1)
+
+                subsys0 = cluster_subsystem.ClusterHLSubSystem(mol0, env_method, hl_method)
+                env_subsys0 = cluster_subsystem.ClusterEnvSubSystem(env_mol0, env_method)
+                sup_mol00 = helpers.concat_mols([mol0, env_mol0])
+                fs_scf_obj0 = helpers.gen_scf_obj(sup_mol00, env_method, grid_level=5)
+                supersystem0 = cluster_supersystem.ClusterSuperSystem([subsys0, env_subsys0], env_method, fs_scf_obj0)
+                supersystem0.init_density()
+                supersystem0.freeze_and_thaw()
+
+                subsys2 = cluster_subsystem.ClusterHLSubSystem(mol2, env_method, hl_method)
+                env_subsys2 = cluster_subsystem.ClusterEnvSubSystem(env_mol2, env_method)
+                sup_mol22 = helpers.concat_mols([mol2, env_mol2])
+                fs_scf_obj2 = helpers.gen_scf_obj(sup_mol22, env_method, grid_level=5)
+                supersystem2 = cluster_supersystem.ClusterSuperSystem([subsys2, env_subsys2], env_method, fs_scf_obj2)
+                supersystem2.init_density()
+                supersystem2.freeze_and_thaw()
+
+                #Num density grad terms
+                num_sub1_den_grad = (subsys2.get_dmat() - subsys0.get_dmat())/(dim_diff*2.)
+                num_sub2_den_grad = (env_subsys2.get_dmat() - env_subsys0.get_dmat())/(dim_diff*2.)
+                
+                s2s = supersystem.sub2sup
                 print ('diff sub 1')
                 print (np.max(np.abs(ao_dmat[0][atm][dim] - num_sub1_den_grad)))
                 print ('diff sub 2')
                 print (np.max(np.abs(ao_dmat[1][atm][dim] - num_sub2_den_grad)))
-                self.assertTrue(np.allclose(ao_dmat[0][atm][dim], num_sub1_den_grad, atol=1e-4))
-                self.assertTrue(np.allclose(ao_dmat[1][atm][dim], num_sub2_den_grad, atol=1e-4))
+                self.assertTrue(np.allclose(ao_dmat[0][atm][dim], num_sub1_den_grad, atol=1e-5))
+                self.assertTrue(np.allclose(ao_dmat[1][atm][dim], num_sub2_den_grad, atol=1e-5))
 
 
         subsys1 = cluster_subsystem.ClusterHLSubSystem(self.cs_twoatm_mol1, env_method, hl_method)
